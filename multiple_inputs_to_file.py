479c5203b30fe9d919ca54750f4b7ba1f38ada4f
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-28 08:09:56 +0800

    added def function and fixed indention

--- multiple_inputs_to_file.py
+++ multiple_inputs_to_file.py
@@ -1,9 +1,8 @@
 #def function
-
+def line():   
 #create empty function
-text_lines=[]
+    text_lines=[]
 #ask user what to write
-
 #add user input to text file
 
 #ask user if they want to add more or no
