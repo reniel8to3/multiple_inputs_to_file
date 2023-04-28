5cbf9b4817f3ed367338eb69dc0ec57c5e37df0a
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-28 08:11:19 +0800

    added user prompt

--- multiple_inputs_to_file.py
+++ multiple_inputs_to_file.py
@@ -3,6 +3,8 @@ def line():
 #create empty function
     text_lines=[]
 #ask user what to write
+    while True:
+        text_input=input('What is/are your favorite song/s?')
 #add user input to text file
 
 #ask user if they want to add more or no
